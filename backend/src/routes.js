c1c38d53deee3903cb4a6f6c12b55dbc35dbf563
JrzenonDev <jrobertoonb@gmail.com>
2020-03-25 21:54:37 -0300

    refact: passando a lógica para controller, melhorando a estrutura do app

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,33 +1,9 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
+const OngController = require('../src/controllers/OngController');
 
 const routes = express.Router();
 
-routes.get('/ongs', async(request, response) => {
-  const ongs = await connection('ongs').select('*');
-
-  return response.json(ongs);
-});
-
-routes.post('/ongs', async(request, response) => {
-
-  const { name, email, whatsapp, city, uf } = request.body;
-
-  // gera um valor de 4bites em hexadecimal para o id para ser aleatório
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  // conexão com bd
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  })
-
-  return response.json({ id });
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
 module.exports = routes;
