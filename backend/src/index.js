92699d0ef53bf77be0fd77173099112e52bd3978
JrzenonDev <jrobertoonb@gmail.com>
2020-03-28 18:52:21 -0300

    chore: adicionando o módulo cors, para limitar o acesso a api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+
+// dev
+app.use(cors());
+
+// producion
+// app.use(cors({
+//   origin: 'http://meuapp.com'
+// }));
 
 app.use(express.json());
 
